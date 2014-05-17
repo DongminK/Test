58a56526ef106849c1ca4d60b050de45a4664dd7
DongminK <kdm0228@gmail.com>
2014-05-17 18:32:48 +0900

    Test Project Create

---
+++ TEST Project/src/dmk/pid/ProcessID.java
@@ -0,0 +1,16 @@
+package dmk.pid;
+
+import java.lang.management.ManagementFactory;
+import java.util.StringTokenizer;
+
+public class ProcessID {
+
+	public static String get() {
+		
+		String runtimeName = ManagementFactory.getRuntimeMXBean().getName(); 
+		StringTokenizer st = new StringTokenizer(runtimeName, "@");
+		String pid = st.nextToken();
+		
+		return pid;
+	}
+}
---
+++ TEST Project/src/dmk/pid/TestDrive.java
@@ -0,0 +1,8 @@
+package dmk.pid;
+
+public class TestDrive {
+
+	public static void main(String[] args) {
+		System.out.println("Current Process ID : " + ProcessID.get());
+	}
+}
